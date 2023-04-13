8395809196cd5a2e2c9af5212eec895169bbf9df
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-04-13 15:00:11 -0700

    updated to match current ex

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const { fetchMyIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
 
-fetchMyIP((error, ip) => {
+const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
-  console.log('It worked! Returned IP:' , ip);
+  console.log('It worked! Returned flyover times:' , passTimes);
 });
